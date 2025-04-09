f145e60c839ed2aae0ffd617c5a28c413640cbbf
SS56Bo <sohamsarkar560@gmail.com>
2025-04-09 17:24:36 +0530

    Initial Commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Greet } from "../wailsjs/go/main/App";
+
+function App() {
+  return (
+    <div className="app-main">
+      <h2>Hello world</h2>
+    </div>
+  );
+}
+
+export default App;
