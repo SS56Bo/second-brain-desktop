853f4664174e06b8b7740a86741a209bc36da77d
SS56Bo <sohamsarkar560@gmail.com>
2025-04-10 00:25:20 +0530

    components

--- frontend/src/components/NewNote.tsx
+++ frontend/src/components/NewNote.tsx
@@ -1,13 +1,39 @@
+import { useState } from "react";
 import "./NewNote.css";
 
 function NewNote() {
+  const [title, setTitle] = useState<string>("");
+  const [desc, setDesc] = useState<string>("");
+
+  // binding for Golang
+  const handleAddNote = () => {
+    console.log("Title: ", title);
+    console.log("Description: ", desc);
+  };
+
   return (
     <div className="main-container">
       <div className="title-container">
-        <h1>Title</h1>
+        <label>Title</label>
+        <input
+          type="text"
+          placeholder="Enter title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
       </div>
       <div className="desc-container">
-        <h1>Description</h1>
+        <label>Description</label>
+        <textarea
+          id="desc-box"
+          placeholder="Enter description"
+          rows={5}
+          value={desc}
+          onChange={(even) => setDesc(even.target.value)}
+        ></textarea>
+      </div>
+      <div className="button-zero">
+        <button onClick={handleAddNote}>Add Note</button>
       </div>
     </div>
   );
